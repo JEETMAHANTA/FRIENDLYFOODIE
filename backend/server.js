b22c114665f10c7a0d89c2de543a1c0ebe2bf482
JEET MAHANTA <jeetmahanta03@gmail.com>
2025-09-18 12:34:02 +0530

    url

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 app.use(
     cors({
         origin: (origin, callback) => {
-            const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+            const allowedOrigins = ['https://friendlyfoodie-frontend.onrender.com', 'https://friendlyfoodie-admin.onrender.com'];
             if (!origin || allowedOrigins.includes(origin)) {
                 callback(null, true);
             } else {
